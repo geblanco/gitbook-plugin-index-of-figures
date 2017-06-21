a72ecbfd5e9aab45fcccf0aba9a4ac7ec7d72fe6
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2017-06-21 14:11:54 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# gitbook-plugin-index-of-figures
-A gitbook plugin to do automatic table of figures and figure name resolution, works in companion with gitbook-plugin-image-captions
+Gitbook Plugin Index of Figures
+=============
+
+A gitbook plugin to do automatic table of figures and figure name resolution, it is inspired by and works in companion with [gitbook-plugin-image-captions](https://github.com/m0n0l0c0/gitbook-plugin-index-of-figures.git).
+
+### Install
+
+Add this to your `book.json`, then run `gitbook install`:
+
+```json
+{
+    "plugins": ["index-of-figures"]
+}
+```
+
+### Setup
+
+In your `book.json` add
+
+```json
+{
+	"pluginsConfig": {
+		"index-of-figures": {
+			"path": "./figures.json"
+		}
+	}
+}
+```
+
+Where `./figures.json` is your figures config file, it is done this way to avoid filling the `book.json` with useless crap about your figures
+
+### Usage
+
+When you want to reference the figure `ANN` in the config file, use the following in the content of a page:
+
+```
+{{ "ANN" | fig }}
+```
+
+You can also add the index of figures table with:
+
+```
+{% figures %} {% endfigures %}
+```
+
+The table of figures should be included in a file called Figures.md
+
+If you name the file anything other than Figures.md, it will break the links from the individual figures to this Figures page (each figure will link to Figures.html#fig- plus the index of that figure).
+
+### Figures settings
+
+Example of `./figures.json`
+
+```json
+{
+	"prefix": "Fig. ",
+	"figures": {
+		"ANN": {
+			"path": "/assets/ANN.png",
+			"description": "An Artificial Neural Network"
+		}
+	}
+} 
+```
+
+* `prefix`: The prefix to put on each figure before the number (eg: Fig, Figure, ...). Defaults to `Fig.`
+* `figures`: An object with all the figures, the name of the figure is to be passed in the content page.
+* `path`: The path to the asset, this serve as a key to find the figure after figcaptions has processed all the figures and assigned numbers automatically, this is the way we substitute the text with the correct figure number.
+* `description`: This is the text used to display in the table of figures 
---
+++ index.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const cheerio = require('cheerio')
+const { readFileSync } = require('fs')
+
+function readJSON(path){
+	const file = readFileSync(path, { encoding: 'utf8'})
+	let ret = {}
+	try{ ret = JSON.parse(file) }
+	catch(e){ ret = {} }
+	finally{ return ret }
+}
+
+function getFigureIndex(figKey, figures){
+	return Object.keys(figures).indexOf(figKey)
+}
+
+// This should be a config var, but for some reason,
+// between the filter and the block, the variable got flushed 
+let insertedFigures = []
+
+module.exports = {
+	book: {
+		assets: './assets',
+		css: ["style.css"]
+	},
+	hooks: {
+    init: function(){
+    	const cfg = readJSON(this.config.get('pluginsConfig')['index-of-figures'].path)
+      this.config.set('figures', cfg.figures)
+      this.config.set('figurePrefix', cfg.prefix || 'Fig.')
+      insertedFigures = []
+    },
+    page: function( page ){
+    	const $ = cheerio.load(page.content)
+    	const prefix = this.config.get('figurePrefix')
+    	const figs = {}
+    	
+    	// Find the figures from fig caption
+			$('figure').each(function(){
+				const num = $(this).attr('id')
+				const src = $('img', this).first().attr('src')
+				figs[src] = num
+			})
+
+			// Find the referenced figure and substitute text with assigned fig number
+			$('a[class*="fig-ref-"]').each(function(){
+				const css = $(this).attr('class')
+				const key = figs[css.replace('fig-ref-', '')]
+				$(this).text(`${prefix} ${key.replace(/(?:[^0-9]+)?([0-9]+(?:\.[0-9])*)/gi, '$1')}`)
+			})
+
+			page.content = $.html()
+    	return page
+    }
+  },
+	filters: {
+		fig: function( key ){
+
+			let ret = '[Figure not found]'
+			const figure = this.config.get('figures')[key]
+
+			if( figure !== undefined ){
+				const index = getFigureIndex(key, this.config.get('figures'))
+				if( index !== undefined ){
+
+					insertedFigures.push(key)
+
+					// ToDo => Find figure indexed number from figcaption
+					ret = `<a href="Figures.html#fig-${index + 1}" class="fig-ref-${figure.path}">Fig. ${index + 1}</a>`
+				}
+			}
+			return ret
+		}
+	},
+	blocks: {
+		figures: {
+			process: function( block ){
+
+				const allFigures = this.config.get('figures')
+				
+				if( insertedFigures.length === 0 ){
+					return ''
+				}
+
+				let result = '<table class="figures">'
+
+				insertedFigures.forEach(function( figKey ){                
+
+					const index = getFigureIndex(figKey, allFigures) + 1
+
+					result += `<tr><td><span class="figure-number" id="fig-${index}">${index}</span></td><td>`
+					result += allFigures[figKey].description
+					result += '</td></tr>'
+				})
+
+				result += '</table>'
+
+				return result
+			}
+		}
+	}
+}
